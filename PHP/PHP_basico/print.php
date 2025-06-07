3fc4439a2dc4b24c28698e905945f1ad442b0283
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-06-07 20:14:39 -0300

    Add files via upload

---
+++ PHP/PHP_basico/print.php
@@ -0,0 +1,16 @@
+<html>
+    <head> 
+        <title>pagina de teste</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <p>
+            <?php
+                echo "ola mundo";
+            ?>
+        </p>
+
+    </body>
+
+</html>
