d4bedad2c4fd0f3ee6bee60dbf77eae58967ae0b
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-07-14 21:08:01 -0300

    Add files via upload

---
+++ PHP_codigos_aleatorios/botao_onclick.php
@@ -0,0 +1,16 @@
+<form>
+        
+        <center>
+        <div>
+        <input type="button" value="cadastra novo solicitante de crédito consignado" onClick="Nova()">
+        </div>
+        <div>
+        <input type="button" value="Consultar crédito" onClick="Novo2()">
+        </div>
+        <div>
+        <input type="button" value="Página Inicial" onClick="NovoInicial()">
+        </div>
+        </center>
+        </form> 
+
+        
